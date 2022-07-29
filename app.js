fe99332eb96044a567e4af41a11aa623699b0e75
Byron-Magofna <Magofnab@oregonstate.edu>
2022-07-28 20:39:21 -0700

    implemented step 3

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ var db = require('./database/db-connector')
 
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT = 6527;                        // Set a port number at the top so it's easy to change in the future
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
+PORT = 6527;                        // Set a port number at the top so it's easy to change in the future
 
 
 
@@ -22,36 +26,10 @@ PORT = 6527;                        // Set a port number at the top so it's easy
 
 
 app.get('/', function(req, res)                                         // This is the basic syntax for what is called a 'route'
-    {
-        // res.send("Byron and Gary's server is running!")         // This function literally sends the string "The server is running!" to the computer
-                                                                        // requesting the web site.
-        // Define our queries
-        query1 = 'DROP TABLE IF EXISTS diagnostic;';
-        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-        query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
-        query4 = 'SELECT * FROM diagnostic;';
+{
+    res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+});                                         // will process this file, before sending the finished HTML to the client.
 
-        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
-
-        // DROP TABLE...
-        db.pool.query(query1, function (err, results, fields){
-
-            // CREATE TABLE...
-            db.pool.query(query2, function(err, results, fields){
-
-                // INSERT INTO...
-                db.pool.query(query3, function(err, results, fields){
-
-                    // SELECT *...
-                    db.pool.query(query4, function(err, results, fields){
-
-                        // Send the results to the browser
-                        res.send(JSON.stringify(results));
-                    });
-                });
-            });
-        });
-    });
 
 
 
